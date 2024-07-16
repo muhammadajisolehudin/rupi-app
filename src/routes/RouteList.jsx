b74ba7821849e39e9030b8dfd1f1f8241e6ab18a
EFENDI <fendikrama56@gmail.com>
2024-07-16 21:44:14 +0700

    feat: add buat password

--- src/routes/RouteList.jsx
+++ src/routes/RouteList.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../pages/auth/LoginPage";
 import BuatPin from "../pages/auth/BuatPin";
 import MasukPin from "../pages/auth/MasukPin";
+import BuatPasswordBaru from "../pages/auth/BuatPassword";
 // import { PinBaru } from "../pages/auth/PinBaru";
 
 export const RouteList = () => {
@@ -11,6 +12,7 @@ export const RouteList = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/buat-pin" element={<BuatPin />} />
         <Route path="/masuk-pin" element={<MasukPin/>}/>
+        <Route path="/buat-password" element={<BuatPasswordBaru/>}/>
       </Routes>
     </BrowserRouter>
   );
