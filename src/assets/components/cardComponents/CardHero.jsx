e2c829b5b50aa2dbae536d2a7aa864240b55a664
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-02 05:36:54 +0700

    feat: update container homepage

--- src/assets/components/cardComponents/CardHero.jsx
+++ src/assets/components/cardComponents/CardHero.jsx
@@ -1,118 +1,105 @@
-import { Card, Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import bannerImage from "../../../images/banner.png";
 import calendarIcon from "../../../icons/calendar.png";
 import avatarWaveImage from "../../../images/avatar and wave.png";
 
 export const CardHero = () => {
   return (
-    <Card sx={{ py: { xs: 4, lg: 8 }, px: { xs: 2, lg: 7 } }}>
-      <Container maxWidth={false} sx={{ p: 0 }}>
-        <Grid container>
-          <Grid item xs={12} lg={7}>
-            <Box
-              sx={{
-                backgroundImage: `url(${bannerImage})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                display: "flex",
-                flexDirection: "row",
-                padding: { xs: "20px 36px", lg: "40px 72px" },
-                height: "248px",
-                position: "relative",
-                borderRadius: "20px",
-                overflow: "hidden",
-              }}
-            >
-              <Box sx={{ flexDirection: "column" }}>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#EDF4FF",
-                    color: "#0A3967",
-                    width: { xs: "100%", lg: "80%" },
-                    padding: "3.5px 27.5px",
-                    marginBottom: "40px",
-                    fontSize: "16px",
-                    fontWeight: "500",
-                    boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
-                    borderRadius: "10px",
-                  }}
-                >
-                  <img
-                    src={calendarIcon}
-                    alt="Calendar"
-                    style={{
-                      width: "24px",
-                      height: "24px",
-                      marginRight: "10px",
-                    }}
-                  />
-                  <Typography
-                    variant="body1"
-                    component="p"
-                    sx={{ marginBottom: 0 }}
-                  >
-                    29 Juli 2024, 10:00 AM
-                  </Typography>
-                </Box>
-                <Typography
-                  variant="h1"
-                  component="h1"
-                  sx={{
-                    color: "#0A3967",
-                    fontSize: { xs: "24px", lg: "32px" },
-                    fontWeight: 700,
-                    marginBottom: "16px",
-                  }}
-                >
-                  Selamat Siang, Samsul!
-                </Typography>
-                <Typography
-                  variant="body1"
-                  component="p"
-                  sx={{
-                    color: "#0066AE",
-                    fontSize: { xs: "14px", lg: "18px" },
-                    fontWeight: 500,
-                    marginBottom: "40px",
-                  }}
-                >
-                  Semoga harimu menyenangkan!
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  position: "absolute",
-                  top: 0,
-                  right: 0,
-                  bottom: 0,
-                  left: "auto",
-                  width: { xs: "100%", lg: "calc(100% - 430px)" },
-                  overflow: "hidden",
-                }}
-              >
-                <img
-                  src={avatarWaveImage}
-                  alt="Avatar and Wave"
-                  style={{
-                    position: "absolute",
-                    right: 0,
-                    top: "50%",
-                    left: "50%",
-                    width: "auto",
-                    height: "100%",
-                    transform: "translate(-50%, -50%)",
-                    objectFit: "cover",
-                  }}
-                />
-              </Box>
-            </Box>
-          </Grid>
-          <Grid item xs={12} lg={5}></Grid>
-        </Grid>
-      </Container>
-    </Card>
+    <Box
+      sx={{
+        backgroundImage: `url(${bannerImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        flexDirection: "row",
+        padding: { xs: "20px 36px", lg: "40px 72px" },
+        height: "248px",
+        position: "relative",
+        borderRadius: "20px",
+        overflow: "hidden",
+      }}
+    >
+      <Box sx={{ flexDirection: "column" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#EDF4FF",
+            color: "#0A3967",
+            width: { xs: "100%", lg: "80%" },
+            padding: "3.5px 27.5px",
+            marginBottom: "40px",
+            fontSize: "16px",
+            fontWeight: "500",
+            boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
+            borderRadius: "10px",
+          }}
+        >
+          <img
+            src={calendarIcon}
+            alt="Calendar"
+            style={{
+              width: "24px",
+              height: "24px",
+              marginRight: "10px",
+            }}
+          />
+          <Typography variant="body1" component="p" sx={{ marginBottom: 0 }}>
+            29 Juli 2024, 10:00 AM
+          </Typography>
+        </Box>
+        <Typography
+          variant="h1"
+          component="h1"
+          sx={{
+            color: "#0A3967",
+            fontSize: { xs: "24px", lg: "32px" },
+            fontWeight: 700,
+            marginBottom: "16px",
+          }}
+        >
+          Selamat Siang, Samsul!
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          sx={{
+            color: "#0066AE",
+            fontSize: { xs: "14px", lg: "18px" },
+            fontWeight: 500,
+            marginBottom: "40px",
+          }}
+        >
+          Semoga harimu menyenangkan!
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: "auto",
+          width: { xs: "100%", lg: "calc(100% - 430px)" },
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src={avatarWaveImage}
+          alt="Avatar and Wave"
+          style={{
+            position: "absolute",
+            right: 0,
+            top: "50%",
+            left: "50%",
+            width: "auto",
+            height: "100%",
+            transform: "translate(-50%, -50%)",
+            objectFit: "cover",
+          }}
+        />
+      </Box>
+    </Box>
   );
 };
--- src/pages/BerandaPage.jsx
+++ src/pages/BerandaPage.jsx
@@ -1,32 +1,46 @@
-import { Box, Button, Typography } from "@mui/material"
-import { Layout } from "./layout"
-import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { Layout } from "./layout";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import { CardHero } from "../assets/components/cardComponents/CardHero";
-import { CardList } from "../assets/components/cardComponents/CardList"
-import { InfoSaldo } from "../assets/components/dashboard/InfoSaldo"
-import { Menu } from "../assets/components/dashboard/Menu"
+import { CardList } from "../assets/components/cardComponents/CardList";
+import { InfoSaldo } from "../assets/components/dashboard/InfoSaldo";
+import { Menu } from "../assets/components/dashboard/Menu";
 
+export const BerandaPage = () => {
+  return (
+    <Layout>
+      <Container
+        maxWidth={false}
+        sx={{ paddingTop: "2rem", paddingBottom: "2rem" }}
+      >
+        <Grid container>
+          <Grid item xs={12} lg={7}>
+            <CardHero />
+          </Grid>
+          <Grid item xs={12} lg={5}></Grid>
+        </Grid>
+      </Container>
 
-
-export const BerandaPage = () =>{
-
-    return(
-        <Layout>
-            {/* <Container sx={{ paddingTop:"2rem", paddingBottom:"2rem" }}> */}
-                <CardHero/>
-                <InfoSaldo/>
-                <Menu/>
-                <Box sx={{ display:"flex", alignItems:"center", justifyContent: "space-between", marginTop:"2rem", marginBottom:"1rem" }}>
-                    <Typography> Transaksi Favorit</Typography>
-                    <Button sx={{ backgroundColor:"#0066AE", color:"white" }}>
-                        <Typography variant="body2" sx={{ textTransform: 'none' }}>Lihat Semua</Typography>
-                        <ChevronRightRoundedIcon />
-                    </Button>
-                </Box>
-                <CardList />
-{/* 
-            </Container> */}
-        </Layout>
-    )
-
-}
+      <InfoSaldo />
+      <Menu />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: "2rem",
+          marginBottom: "1rem",
+        }}
+      >
+        <Typography> Transaksi Favorit</Typography>
+        <Button sx={{ backgroundColor: "#0066AE", color: "white" }}>
+          <Typography variant="body2" sx={{ textTransform: "none" }}>
+            Lihat Semua
+          </Typography>
+          <ChevronRightRoundedIcon />
+        </Button>
+      </Box>
+      <CardList />
+    </Layout>
+  );
+};
