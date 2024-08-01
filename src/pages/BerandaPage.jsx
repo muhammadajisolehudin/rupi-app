9c028e5c134d1b619e3939d1086b8231e35cb9a0
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-02 04:24:46 +0700

    feat: add hero component

--- src/pages/BerandaPage.jsx
+++ src/pages/BerandaPage.jsx
@@ -1,7 +1,7 @@
-import { Box, Button, Card, Container, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import { Layout } from "./layout"
-import user from "../assets/img/icons/user.png"
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { CardHero } from "../assets/components/cardComponents/CardHero";
 import { CardList } from "../assets/components/cardComponents/CardList"
 import { InfoSaldo } from "../assets/components/dashboard/InfoSaldo"
 import { Menu } from "../assets/components/dashboard/Menu"
@@ -12,16 +12,8 @@ export const BerandaPage = () =>{
 
     return(
         <Layout>
-            <Container sx={{ paddingTop:"2rem", paddingBottom:"2rem" }}>
-                {/* content */}
-                 <Card sx={{ minWidth: 275, height:70, bgcolor:"transparent",display:"flex", alignItems:"center" }}>
-                    <Box sx={{ display:"flex", alignItems:"center", p:2}}>
-                        <img src={user} alt="user" />
-                        <Typography variant="body2" sx={{ marginLeft:2 }}>
-                            Selamat Siang, Samsul
-                        </Typography>
-                    </Box>
-                </Card>
+            {/* <Container sx={{ paddingTop:"2rem", paddingBottom:"2rem" }}> */}
+                <CardHero/>
                 <InfoSaldo/>
                 <Menu/>
                 <Box sx={{ display:"flex", alignItems:"center", justifyContent: "space-between", marginTop:"2rem", marginBottom:"1rem" }}>
@@ -32,8 +24,8 @@ export const BerandaPage = () =>{
                     </Button>
                 </Box>
                 <CardList />
-
-            </Container>
+{/* 
+            </Container> */}
         </Layout>
     )
 
