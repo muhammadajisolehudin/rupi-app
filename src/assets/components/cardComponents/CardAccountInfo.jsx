d6b5fc62641f2ded8f4b663e134149b5152cc8e0
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-08-02 11:58:33 +0700

    fix: flow tarik tunai, design form, aria

--- src/assets/components/cardComponents/CardAccountInfo.jsx
+++ src/assets/components/cardComponents/CardAccountInfo.jsx
@@ -4,14 +4,14 @@ import { PropTypes } from "prop-types";
 
 export const CardAccountInfo = ({ accountNumber, balance }) => {
 	return (
-		<Card component={Paper} elevation={4} sx={{ p: 2, my: 2 }}>
+		<Card component={Paper} elevation={2} sx={{ p: 2, my: 2 }}>
 			<div>
-				<Typography>
+				<Typography aria-label="nomor rekening">
 					<span style={{ color: "grey" }}>No. Rekening:</span> <span>{accountNumber}</span>
 				</Typography>
 			</div>
 			<div>
-				<Typography>
+				<Typography aria-label="total balance rekening">
 					<span style={{ color: "grey" }}>Saldo:</span> <span>Rp {balance.toLocaleString("id-ID")}</span>
 				</Typography>
 			</div>
--- src/assets/components/tarikSetorComponents/TarikTunai.jsx
+++ src/assets/components/tarikSetorComponents/TarikTunai.jsx
@@ -1,20 +1,22 @@
 // import React from "react";
 
-import { Box, Card, Paper, TextField, Typography } from "@mui/material";
+import { Box, Card, Container, Paper, TextField, Typography } from "@mui/material";
 import { CardAccountInfo } from "../cardComponents/CardAccountInfo";
-import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
+import { Form, useFormik } from "formik";
 import * as Yup from "yup";
-import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ShareIcon from "@mui/icons-material/Share";
 import checklistIcon from "../../img/checklist-icon.png";
 import logoIcon from "/logo.png";
+import { Link } from "react-router-dom";
 
 const RequestNominalForm = ({ onNext }) => {
 	const formik = useFormik({
 		initialValues: {
 			nominal: "",
 			namaToken: "",
+			accountNumber: "5667 2323 1444 5554",
+			balance: 5000000,
 		},
 		validationSchema: Yup.object({
 			nominal: Yup.number().min(50000, "Nominal Tarik Tunai minimal IDR 50.000").required("Required"),
@@ -27,87 +29,81 @@ const RequestNominalForm = ({ onNext }) => {
 
 	return (
 		<>
-			<Formik
-				initialValues={formik.initialValues}
-				validationSchema={formik.validationSchema}
-				onSubmit={formik.onSubmit}
-			>
-				<Typography variant="h6" sx={{ mt: 5 }}>
-					Rekening Tujuan
-				</Typography>
-				<CardAccountInfo accountNumber={"5667 2323 1444 5554"} balance={5000000} />
+			<Container>
+				<form onSubmit={formik.handleSubmit}>
+					<Typography variant="h6" sx={{ mt: 5 }}>
+						Rekening Tujuan
+					</Typography>
+					<CardAccountInfo accountNumber={"5667 2323 1444 5554"} balance={5000000} />
 
-				<Box sx={{ bgcolor: "#EFEFEF", borderRadius: 1, p: 1, mt: 4 }}>
-					<Typography>Nominal Bayar</Typography>
+					<Box sx={{ bgcolor: "#EFEFEF", borderRadius: 1, p: 1, mt: 4 }}>
+						<Typography>Nominal Bayar</Typography>
+						<TextField
+							aria-label="input nominal"
+							margin="normal"
+							name="nominal"
+							type="number"
+							id="nominal"
+							placeholder="Rp 0"
+							autoComplete="current-nominal"
+							fullWidth
+							required
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.nominal}
+						/>
+					</Box>
+					{formik.touched.nominal && formik.errors.nominal ? (
+						<Typography sx={{ fontSize: 10, color: "red", mt: 2 }}>{formik.errors.nominal}</Typography>
+					) : (
+						<Typography sx={{ fontSize: 12, color: "grey", mt: 2 }}>
+							Nominal Tarik Tunai minimal IDR 50.000
+						</Typography>
+					)}
+
+					<Typography sx={{ mt: 4 }}>Beri Nama Token</Typography>
 					<TextField
-						aria-label="input nominal"
+						aria-label="tambahkan nama token tarik tunai"
 						margin="normal"
-						name="nominal"
-						type="number"
-						id="nominal"
-						placeholder="Rp 0"
+						name="namaToken"
+						type="text"
+						id="namaToken"
+						placeholder="Tambahkan nama token tarik tunai"
 						autoComplete="current-nominal"
 						fullWidth
-						required
 						onChange={formik.handleChange}
 						onBlur={formik.handleBlur}
-						value={formik.values.nominal}
+						value={formik.values.namaToken}
 					/>
-				</Box>
-				{formik.touched.nominal && formik.errors.nominal ? (
-					<Typography sx={{ fontSize: 10, color: "red", mt: 2 }}>{formik.errors.nominal}</Typography>
-				) : (
-					<Typography sx={{ fontSize: 12, color: "grey", mt: 2 }}>
-						Nominal Tarik Tunai minimal IDR 50.000
-					</Typography>
-				)}
+					{formik.touched.namaToken && formik.errors.namaToken ? (
+						<Typography sx={{ fontSize: 10, color: "red", mt: 2 }}>{formik.errors.namaToken}</Typography>
+					) : null}
 
-				<Typography sx={{ mt: 4 }}>Beri Nama Token</Typography>
-				<TextField
-					aria-label="tambahkan nama token tarik tunai"
-					margin="normal"
-					name="namaToken"
-					type="text"
-					id="namaToken"
-					placeholder="Tambahkan nama token tarik tunai"
-					autoComplete="current-nominal"
-					fullWidth
-					onChange={formik.handleChange}
-					onBlur={formik.handleBlur}
-					value={formik.values.namaToken}
-				/>
-				{formik.touched.namaToken && formik.errors.namaToken ? (
-					<Typography sx={{ fontSize: 10, color: "red", mt: 2 }}>{formik.errors.namaToken}</Typography>
-				) : null}
-
-				<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", my: 5 }}>
-					<button
-						type="submit"
-						style={{
-							borderRadius: "15px",
-							border: 0,
-							padding: "8px",
-							width: "500px",
-							backgroundColor: "#0066AE",
-							color: "white",
-						}}
-					>
-						Lanjutkan
-					</button>
-				</Box>
-			</Formik>
+					<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", my: 5 }}>
+						<button
+							type="submit"
+							style={{
+								borderRadius: "15px",
+								border: 0,
+								padding: "8px",
+								width: "500px",
+								backgroundColor: "#0066AE",
+								color: "white",
+							}}
+							aria-label="submit request tarik tunai form"
+						>
+							Lanjutkan
+						</button>
+					</Box>
+				</form>
+			</Container>
 		</>
 	);
 };
 
 const KonfirmasiForm = ({ onNext }) => {
 	const formik = useFormik({
-		initialValues: {
-			pin: "",
-		},
-		validationSchema: Yup.object({
-			pin: Yup.string().required("PIN is required").min(6, "PIN must be at least 4 characters"),
-		}),
+		initialValues: {},
 		onSubmit: (values) => {
 			onNext(values);
 		},
@@ -115,127 +111,190 @@ const KonfirmasiForm = ({ onNext }) => {
 
 	return (
 		<>
-			<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", my: 2 }}>
-				<img src={logoIcon} alt="" style={{ marginRight: "5px" }} />
-				<Typography variant={"h5"} sx={{ fontWeight: "bold" }}>
-					Rupi App
-				</Typography>
-			</Box>
-			<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", mb: 4, mt: 2 }}>
-				<Typography variant={"h6"} sx={{ fontWeight: "bold" }}>
-					Konfirmasi Tarik Tunai
-				</Typography>
-			</Box>
+			<Container>
+				<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", my: 2 }}>
+					<img src={logoIcon} alt="" style={{ marginRight: "5px" }} />
+					<Typography variant={"h5"} sx={{ fontWeight: "bold" }}>
+						Rupi App
+					</Typography>
+				</Box>
+				<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", mb: 4, mt: 2 }}>
+					<Typography variant={"h6"} sx={{ fontWeight: "bold" }} aria-label="konfirmasi tarik tunai">
+						Konfirmasi Tarik Tunai
+					</Typography>
+				</Box>
 
-			<Box sx={{ my: 2 }}>
+				<Box sx={{ my: 2 }}>
+					<hr />
+					<Box sx={{ display: "flex", justifyContent: "space-between" }} aria-label="nominal penarikan">
+						<Typography>Nominal Penarikan</Typography>
+						<Typography sx={{ fontWeight: "bold" }}>Rp.100.000</Typography>
+					</Box>
+					<Box
+						sx={{ display: "flex", justifyContent: "space-between" }}
+						aria-label="nama token penarikan"
+					>
+						<Typography>Nama Token</Typography>
+						<Typography sx={{ fontWeight: "bold" }}>Tarik Senin</Typography>
+					</Box>
+				</Box>
 				<hr />
-				<Box sx={{ display: "flex", justifyContent: "space-between" }}>
-					<Typography>Nominal Penarikan</Typography>
-					<Typography sx={{ fontWeight: "bold" }}>Rp.100.000</Typography>
+				<Box sx={{ my: 3 }}>
+					<Typography variant="h6" sx={{ mt: 5 }} aria-label="sumber dana rupiah">
+						Sumber Rupiah
+					</Typography>
+					<CardAccountInfo accountNumber={"5667 2323 1444 5554"} balance={5000000} />
 				</Box>
-				<Box sx={{ display: "flex", justifyContent: "space-between" }}>
-					<Typography>Nama Token</Typography>
-					<Typography sx={{ fontWeight: "bold" }}>Tarik Senin</Typography>
-				</Box>
-			</Box>
-			<hr />
-			<Box sx={{ my: 3 }}>
-				<Typography variant="h6" sx={{ mt: 5 }}>
-					Sumber Rupiah
-				</Typography>
-				<CardAccountInfo accountNumber={"5667 2323 1444 5554"} balance={5000000} />
-			</Box>
-			<hr />
+				<hr />
 
-			<Formik
-				initialValues={formik.initialValues}
-				validationSchema={formik.validationSchema}
-				onSubmit={formik.onSubmit}
-			>
-				<Box
-					sx={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						flexDirection: "column",
-						my: 5,
-					}}
-				>
-					<Box sx={{ my: 3, color: "grey" }}>Token hanya valid selama 1 jam</Box>
-					<button
-						type="submit"
-						style={{
-							borderRadius: "15px",
-							border: 0,
-							padding: "8px",
-							width: "500px",
-							backgroundColor: "#0066AE",
-							color: "white",
+				<form onSubmit={formik.handleSubmit}>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							flexDirection: "column",
+							my: 5,
 						}}
 					>
-						Lanjutkan
-					</button>
-				</Box>
-			</Formik>
+						<Box sx={{ my: 3, color: "grey" }}>Token hanya valid selama 1 jam</Box>
+						<button
+							type="submit"
+							style={{
+								borderRadius: "15px",
+								border: 0,
+								padding: "8px",
+								width: "500px",
+								backgroundColor: "#0066AE",
+								color: "white",
+							}}
+							aria-label="submit confirmation form"
+						>
+							Lanjutkan
+						</button>
+					</Box>
+				</form>
+			</Container>
 		</>
 	);
 };
 
 const InputPinForm = ({ onSubmit }) => {
-	const formik = useFormik({
-		initialValues: {
-			pin: "",
-		},
-		validationSchema: Yup.object({
-			pin: Yup.string().required("PIN is required").min(6, "PIN must be at least 4 characters"),
-		}),
-		onSubmit: (values) => {
-			onSubmit(values);
-		},
-	});
+	const [pin, setPin] = useState(["", "", "", "", "", ""]);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+			const key = event.key;
+			if (key === "Enter") {
+				handleButtonClick();
+			}
+
+			if (key >= "0" && key <= "9") {
+				const index = pin.findIndex((entry) => entry === "");
+				if (index !== -1) {
+					const newpin = [...pin];
+					newpin[index] = key;
+					setPin(newpin);
+				}
+			} else if (key === "Backspace") {
+				const index = pin
+					.slice()
+					.reverse()
+					.findIndex((entry) => entry !== "");
+				if (index !== -1) {
+					const newpin = [...pin];
+					const originalIndex = pin.length - 1 - index;
+					newpin[originalIndex] = "";
+					setPin(newpin);
+				}
+			}
+		};
+
+		document.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, [pin]);
+
+	const pinSchema = Yup.string()
+		.length(6, "PIN harus terdiri dari 6 digit")
+		.required("PIN diperlukan");
+
+	const handleButtonClick = () => {
+		const pinValue = pin.join("");
+		try {
+			pinSchema.validateSync(pinValue);
+			setError("");
+			onSubmit({ pin: String(pinValue, 10) });
+		} catch (validationError) {
+			setError(validationError.message);
+		}
+	};
 
 	return (
-		<>
-			<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", my: 4 }}>
-				<Typography variant={"h5"} sx={{ color: "#0A3967", fontWeight: "bold" }}>
-					Masukkan PIN
-				</Typography>
+		<Box
+			sx={{
+				paddingY: 10,
+				height: "100%",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Typography variant="h5" sx={{ fontWeight: 600, mx: "auto", my: 5 }}>
+				Masukkan PIN
+			</Typography>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					flexDirection: "row",
+					gap: 2,
+					mt: 5,
+				}}
+			>
+				{[...Array(6)].map((_, index) => (
+					<Box
+						key={index}
+						sx={{
+							borderRadius: "50%",
+							bgcolor: pin[index] ? "#0066AE" : "#B3B3B3",
+							width: 30,
+							height: 30,
+						}}
+					/>
+				))}
 			</Box>
-
-			{/* <MasukPin /> */}
-			<TextField
-				aria-label="masukkan pin"
-				margin="normal"
-				name="pin"
-				type="text"
-				id="pin"
-				placeholder="masukan pin"
-				autoComplete="current-nominal"
-				fullWidth
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-				value={formik.values.pin}
-			/>
-			<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", my: 5 }}>
+			{error && (
+				<Typography color="error" sx={{ mt: 3, fontSize: "14px" }}>
+					{error}
+				</Typography>
+			)}
+			<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", my: 10 }}>
 				<button
-					type="submit"
+					onClick={handleButtonClick}
+					type="button"
 					style={{
-						borderRadius: "15px",
+						borderRadius: "10px",
 						border: 0,
-						padding: "8px",
-						width: "500px",
+						padding: "11px",
+						width: "800px",
 						backgroundColor: "#0066AE",
 						color: "white",
 					}}
+					aria-label="submit pin"
 				>
 					Lanjutkan
 				</button>
 			</Box>
-		</>
+		</Box>
 	);
 };
 
-const GetToken = () => {
+const TokenTarik = () => {
 	return (
 		<>
 			<Box
@@ -326,6 +385,7 @@ const GetToken = () => {
 						backgroundColor: "#0066AE",
 						color: "white",
 					}}
+					aria-label="button make new token"
 				>
 					Buat Token Baru
 				</button>
@@ -335,84 +395,26 @@ const GetToken = () => {
 };
 
 export const TarikTunai = () => {
-	const formik = useFormik({
-		initialValues: {
-			nominal: "",
-			namaToken: "",
-		},
-		validationSchema: Yup.object({
-			nominal: Yup.number().min(50000, "Nominal Tarik Tunai minimal IDR 50.000").required("Required"),
-			namaToken: Yup.string().min(6, "Must be at least 6 characters"),
-		}),
-		onSubmit: (values) => {
-			alert(JSON.stringify(values, null, 2));
-		},
-	});
-	const handleClick = () => {
-		console.log("handle lanjut clicked");
+	const [step, setStep] = useState(1);
+	const [formData, setFormData] = useState({});
+
+	const handleNext = (values) => {
+		console.log("handleNext called with values:", values);
+		setFormData((prevData) => ({ ...prevData, ...values }));
+		setStep((prevStep) => prevStep + 1);
 	};
+
+	const handleSubmit = (values) => {
+		console.log("Final Submission", { ...formData, ...values });
+		setStep((prevStep) => prevStep + 1);
+	};
+
 	return (
 		<>
-			<Typography variant="h6" sx={{ mt: 5 }}>
-				Rekening Tujuan
-			</Typography>
-			<CardAccountInfo accountNumber={"5667 2323 1444 5554"} balance={5000000} />
-			<Box sx={{ bgcolor: "#EFEFEF", borderRadius: 1, p: 1, mt: 4 }}>
-				<Typography>Nominal Bayar</Typography>
-				<TextField
-					aria-label="input nominal"
-					margin="normal"
-					name="nominal"
-					type="number"
-					id="nominal"
-					placeholder="Rp 0"
-					autoComplete="current-nominal"
-					fullWidth
-					required
-					onChange={formik.handleChange}
-					onBlur={formik.handleBlur}
-					value={formik.values.nominal}
-				/>
-			</Box>
-			{formik.touched.nominal && formik.errors.nominal ? (
-				<Typography sx={{ fontSize: 10, color: "red", mt: 2 }}>{formik.errors.nominal}</Typography>
-			) : (
-				<Typography sx={{ fontSize: 12, color: "grey", mt: 2 }}>
-					Nominal Tarik Tunai minimal IDR 50.000
-				</Typography>
-			)}
-			<Typography sx={{ mt: 4 }}>Beri Nama Token</Typography>
-			<TextField
-				aria-label="tambahkan nama token tarik tunai"
-				margin="normal"
-				name="namaToken"
-				type="text"
-				id="namaToken"
-				placeholder="Tambahkan nama token tarik tunai"
-				autoComplete="current-nominal"
-				fullWidth
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-				value={formik.values.namaToken}
-			/>
-			{formik.touched.namaToken && formik.errors.namaToken ? (
-				<Typography sx={{ fontSize: 10, color: "red", mt: 2 }}>{formik.errors.namaToken}</Typography>
-			) : null}
-			<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", my: 5 }}>
-				<button
-					onClick={handleClick}
-					style={{
-						borderRadius: "15px",
-						border: 0,
-						padding: "8px",
-						width: "500px",
-						backgroundColor: "#0066AE",
-						color: "white",
-					}}
-				>
-					Lanjutkan
-				</button>
-			</Box>
+			{step === 1 && <RequestNominalForm onNext={handleNext} />}
+			{step === 2 && <KonfirmasiForm onNext={handleNext} />}
+			{step === 3 && <InputPinForm onSubmit={handleSubmit} />}
+			{step === 4 && <TokenTarik />}
 		</>
 	);
 };
