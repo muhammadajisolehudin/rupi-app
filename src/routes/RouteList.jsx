994efacbdb9961d4567cb850b4f4abcd6e6934b2
EFENDI <fendikrama56@gmail.com>
2024-08-04 17:43:23 +0700

    update qr terima sukses - filter

--- src/routes/RouteList.jsx
+++ src/routes/RouteList.jsx
@@ -4,6 +4,8 @@ import { LoginPage } from '../pages/auth/LoginPage';
 import { BerandaPage } from '../pages/BerandaPage';
 import { TransferRupiahPage } from '../pages/TransferRupiahPage';
 import { NotifikasiBerhasilPage } from '../pages/NotifikasiBerhasilPage';
+import { QRTerimaTransfer } from '../pages/QRTerimaTransfer';
+import  QRRiwayatTransfer  from '../pages/QRRiwayatTransfer';
 import BuatPin from "../pages/auth/BuatPin";
 import MasukPin from "../pages/auth/MasukPin";
 import BuatPasswordBaru from "../pages/auth/BuatPassword";
@@ -17,6 +19,8 @@ export const RouteList = () => {
           <Route path="/beranda" element={ <BerandaPage/>}/>
           <Route path="/transfer-rupiah" element={ <TransferRupiahPage/>}/>
           <Route path="/notif-success" element={ <NotifikasiBerhasilPage/>}/>
+          <Route path="/QR-terima-transfer" element={ <QRTerimaTransfer/>}/>
+          <Route path="/QR-terima-transfer/riwayat" element={ <QRRiwayatTransfer/>}/>
 
           <Route path="/login" element={<LoginPage />} />
           <Route path="/buat-pin" element={<BuatPin />} />
