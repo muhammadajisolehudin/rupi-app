1c8c0f7e55f6090ecb6e4102412899490dbd5ee5
muhammadajisolehudin <muhammadajisolehudin@gmail.com>
2024-08-09 22:52:44 +0700

    fix : solving bug deploy

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-
-//   optimizeDeps: {
-//     exclude: ['x-date-pickers']
-//   }
-// })
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src", // Path relatif ke root proyek
-    },
-  },
+
   optimizeDeps: {
-    exclude: ["x-date-pickers"],
-  },
-});
+    exclude: ['x-date-pickers']
+  }
+})
