8e435a27ca68591ce7b28b7646f90090d49dba84
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-16 11:53:34 +0700

    feat(api): generate qr code for terima transfer v2
    all done, except:
    - hidden some digit in account number is error for no reason

---
+++ src/pages/QRTerimaTransfer/QRTerimaTransferCode.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import useGenerateTransactionQR from '../../services/qr-transfer/generate-qr';
+
+export const QRTerimaTransferCode = () => {
+    const [qrCode, setQrCode] = useState(null);
+    const { mutate, isLoading, error } = useGenerateTransactionQR();
+
+    useEffect(() => {
+        mutate(undefined, {
+            onSuccess: (response) => {
+                if (response.data.success) {
+                    setQrCode(response.data.data);
+                }
+            },
+            onError: (err) => {
+                console.error('Error generating QR code:', err);
+            },
+        });
+    }, [mutate]);
+
+    return (
+        <div>
+            {!qrCode && !isLoading && !error && <p>Generating your QR Code...</p>}
+
+            {error && <p>Something went wrong while generating the QR code. Please try again later</p>}
+
+            {qrCode && !isLoading && (
+                <div>
+                    <img src={qrCode} alt="QR Code" style={{ width: '300px', height: '300px', borderRadius: '10px' }} />
+                </div>
+            )}
+        </div>
+    );
+};
